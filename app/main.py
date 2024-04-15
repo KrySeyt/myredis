db2f2109c6ec269fbea149702dd8236dd23c79ba
Vladimir Kibisov <kryseyt@gmail.com>
2024-04-15 20:47:17 +0300

    echo

--- app/main.py
+++ app/main.py
@@ -6,27 +6,26 @@ def commands_handler(conn: socket.socket) -> None:
     while command := conn.recv(1024):
         match parse_redis_command(command):
             case ["PING"]:
-                cmd = ping()
-                print(cmd)
-                conn.send(cmd)
+                conn.send(ping())
 
-            # case "ECHO", value:
-            #     print(value)
-            case _:
-                print("OTHER")
-
+            case ["ECHO", value]:
+                conn.send(echo(value))
 
 
 def parse_redis_command(serialized_command: bytes) -> list[str]:
     command = serialized_command.decode("utf-8")
     commands = command.strip().split("\r\n")
-    cmds = [cmd.upper() for cmd in commands if cmd[0] not in ("*", "$")]
-    print(cmds)
+    cmds = [cmd for cmd in commands if cmd[0] not in ("*", "$")]
+    cmds[0] = cmds[0].upper()
     return cmds
 
 
 def ping() -> bytes:
     return "+PONG\r\n".encode("utf-8")
+
+
+def echo(value: str) -> bytes:
+    return f"${len(value)}\r\n{value}\r\n".encode("utf-8")
 
 
 def main():
