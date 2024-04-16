8ec10b6e4a14254ce44854a01a34884da0adaf4e
Vladimir Kibisov <kryseyt@gmail.com>
2024-04-16 17:46:20 +0300

    info

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Record:
     expire: float | None
 
 
-COMMANDS = {"PING", "ECHO", "SET", "PX", "GET"}
+COMMANDS = {"PING", "ECHO", "SET", "PX", "GET", "INFO", "REPLICATION"}
 
 storage: dict[str, Any] = dict()
 
@@ -34,6 +34,9 @@ def commands_handler(conn: socket.socket) -> None:
 
             case ["GET", str(key)]:
                 conn.send(get(key))
+
+            case ["INFO", "REPLICATION"]:
+                conn.send(info())
 
             case _:
                 print(parsed_command)
@@ -81,12 +84,16 @@ def echo(value: str) -> bytes:
     return f"${len(value)}\r\n{value}\r\n".encode("utf-8")
 
 
+def info() -> bytes:
+    return "$11\r\nrole:master\r\n".encode("utf-8")
+
+
 def main() -> None:
     arg_parser = argparse.ArgumentParser(description="My Redis")
     arg_parser.add_argument("--port", default=6379, type=int)
     args = arg_parser.parse_args()
 
-    server_socket = socket.create_server(("localhost", int(args.port)), reuse_port=True)
+    server_socket = socket.create_server(("localhost", args.port), reuse_port=True)
 
     with ThreadPoolExecutor() as pool:
         while True:
